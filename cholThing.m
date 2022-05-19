676382a4a834d7d85e6074d75b6bd45e35557c85
Emmanuel Katwikirize <ekatwikz@gmail.com>
2022-05-19 15:42:33 +0200

    more changes

--- cholThing.m
+++ cholThing.m
@@ -1,32 +1,40 @@
-A = [10 2 1 0 0; 
-   2 5 3 0 0;
-   1 3 10 3 1;
-   0 0 3 4 2;
-   0 0 1 2 2];
+format short;
 
-[n, ~] = size(A);
-LowerMatrix = zeros(n); % upper matrix
+A = [10 4 1 0 0;
+      4 5 3 2 0;
+      1 3 10 3 1;
+      0 2 3 4 2;
+      0 0 1 2 2];
 
-% for each column
-for k = 1:n
-    % sum over (l_k,j)^2, for the diagonal element
-    sumForDiagonal = 0;
-    for j=1:k - 1
-        sumForDiagonal = sumForDiagonal + LowerMatrix(k, j) * LowerMatrix(k, j);
-    end
-    % set diagonal element
-    LowerMatrix(k, k) = sqrt(A(k, k) - sumForDiagonal);
+[~, n] = size(A);
+LowerMatrix = zeros(n); % lower matrix
+matrixDet = 1;
 
-    for i = k + 1:n
-        sumForNonDiagonal = 0
-        for j = 1:k-1
-            sumForNonDiagonal = LowerMatrix(i, j) * LowerMatrix(k, j)
+%from chol-banach algo on wikipedia:
+% for each row...
+for i = 1 : n
+    % ... and for each column,
+    % we can skip every value that's too far below the diagonal because
+    % it'll be zero (TODO: prove)
+    for j = max(i - 2 , 1) : i
+        sum = 0;
+
+        % we also don't need to sum from too far above
+        for k = max(j - 2, 1) : j - 1
+            %fprintf("L_(%d,%d):%.3f\tL_(%d,%d):%.3f\n", i, k, LowerMatrix(i, k), j, k, LowerMatrix(j, k));
+            sum = sum + LowerMatrix(i, k) * LowerMatrix(j, k);
         end
-        % set non-diagonal:
-        LowerMatrix(i, k) = (A(i, k) - sumForNonDiagonal) / LowerMatrix(k, k)
+
+        %fprintf("A(%d,%d):%.3f\n\n", i, j, A(i, j));
+        if i == j
+            LowerMatrix(i, i) = sqrt(A(i, i) - sum);
+            matrixDet = matrixDet * LowerMatrix(i, i).^2;
+        else
+            LowerMatrix(i, j) = 1.0 / LowerMatrix(j , j) * (A(i, j) - sum);
+        end
     end
 end
 
- LowerMatrix * transpose(LowerMatrix)
-A
-LowerMatrix
+LLTerror = LowerMatrix * transpose(LowerMatrix) - A;
+DeterminantError = det(A) - matrixDet;
+LowerMatrix;
