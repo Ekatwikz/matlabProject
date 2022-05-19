192659a299be270902ee86a23195ba5a552c1dc5
Emmanuel Katwikirize <ekatwikz@gmail.com>
2022-05-19 16:04:05 +0200

    changed accidental comment

--- restrictedLowerMatrixToSquare.m
+++ restrictedLowerMatrixToSquare.m
@@ -3,15 +3,15 @@ function [ lowerSquareMatrix ] = restrictedLowerMatrixToSquare ( restrictedLower
     lowerSquareMatrix = zeros(n);
 
     for i = 1 : n
+        % 3rd column is the diagonal
         lowerSquareMatrix(i, i) = restrictedLowerMatrix(i, 3);
     
-        % here we're getting values from the left of the diagonal,
-        % if we can.
-        % we may note that the symmetry allows this to be implemented in other
-        % ways too
+        % 2nd column is right below
         if i > 1
             lowerSquareMatrix(i, i - 1) = restrictedLowerMatrix(i, 2);
         end
+
+        % 1st column is below that
         if i > 2
             lowerSquareMatrix(i, i - 2) = restrictedLowerMatrix(i, 1);
         end
