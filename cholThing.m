c6728bb0de819f62784e06606876627bcdb77963
Emmanuel Katwikirize <ekatwikz@gmail.com>
2022-05-19 15:47:03 +0200

    changed some variable names, testing something

--- cholThing.m
+++ cholThing.m
@@ -10,27 +10,36 @@ matrixDet = 1;
 
 for i = 1 : n
     for j = max(4 - i, 1) : 3
-        %fprintf("L_(%d,%d):%.3f\tL_(%d,%d):%.3f\n", i, k, LowerMatrix(i, k), j, k, LowerMatrix(j, k));
-        %k = max(4 - i, 1);
+        %fprintf("dep:");
         if j == 1 || i == 1 || i == 2 && j == 2
             % zero dependencies
+            fprintf("0 ");
             sum = 0;
-        elseif j == 2 || i == 2 && j == 3
+        elseif j == 2
+            % depends on one to the left and one to the top left
+            fprintf("1 ");
             sum = LowerMatrix(i, j - 1) * LowerMatrix(i - 1, j);
         elseif j == 3
+            fprintf("2 ");
             % depends on this row
             sum = LowerMatrix(i, 1).^2 + LowerMatrix(i, 2).^2;
         end
-        fprintf("i:%d j:%d sum:%d\n", i, j, sum);
 
-        if i == 3 % if we're on the diagonal
+        if j == 3 % if we're on the diagonal
             LowerMatrix(i, j) = sqrt(A(i, j) - sum);
             matrixDet = matrixDet * LowerMatrix(i, j).^2;
         else % non-diagonal
-            LowerMatrix(i, j) = 1.0 / LowerMatrix(j , 3) * (A(i, j) - sum);
+            if j == 1
+                LM = LowerMatrix(i - 2 , 3);
+            elseif j == 2
+                LM = LowerMatrix(i - 1 , 3);
+            end
+            %fprintf("tmp:%d ", LM);
+            LowerMatrix(i, j) = 1.0 / LM * (A(i, j) - sum);
         end
 
-        %fprintf("A(%d,%d):%.3f\n\n", i, j, A(i, j));
+        %fprintf("sum:%d A(%d,%d):%.3f\n", sum, i, j, A(i, j));
+        fprintf("A(%d,%d):%.3f\n", i, j, LowerMatrix(i, j));
     end
 end
 
